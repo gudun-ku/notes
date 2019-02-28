42d6d87db0781eca3f5cabc43cb0bc286770c7c7
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-02-28 19:46:00 +0300

    Main activity added

--- app/src/main/java/com/beloushkin/android/learn/notes/NavigationActivity.java
+++ app/src/main/java/com/beloushkin/android/learn/notes/NavigationActivity.java
@@ -17,14 +17,11 @@ public class NavigationActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_tasks:
+                    mTextMessage.setText(R.string.title_task);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_notes:
+                    mTextMessage.setText(R.string.title_note);
                     return true;
             }
             return false;
