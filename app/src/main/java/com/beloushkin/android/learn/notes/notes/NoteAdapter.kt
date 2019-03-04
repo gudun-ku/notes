69ee6f9b6673459482049c8bed20d52650e7e9bd
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-03-04 10:02:16 +0300

    NoteAdapter and base abstraction for adapters

--- app/src/main/java/com/beloushkin/android/learn/notes/notes/NoteAdapter.kt
+++ app/src/main/java/com/beloushkin/android/learn/notes/notes/NoteAdapter.kt
@@ -5,25 +5,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.beloushkin.android.learn.notes.R
+import com.beloushkin.android.learn.notes.foundations.BaseRecyclerAdapter
 import com.beloushkin.android.learn.notes.models.Note
-import com.beloushkin.android.learn.notes.tasks.TaskAdapter
 import kotlinx.android.synthetic.main.item_note.view.*
 
 class NoteAdapter(
-    private val notesList: MutableList<Note> = mutableListOf()
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    notesList: MutableList<Note> = mutableListOf()
+) : BaseRecyclerAdapter<Note>(notesList) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder  =
     ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false))
 
-    override fun getItemCount() = notesList.size
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as NoteAdapter.ViewHolder).onBind(notesList.get(position))
-    }
-
-    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        fun onBind(note: Note) {
-            view.noteDescriptionView.text = note.description
+    class ViewHolder(view: View) : BaseViewHolder<Note>(view) {
+        override fun onBind(data: Note) {
+            view.descriptionView.text = data.description
         }
     }
 }
--- app/src/main/java/com/beloushkin/android/learn/notes/tasks/TaskAdapter.kt
+++ app/src/main/java/com/beloushkin/android/learn/notes/tasks/TaskAdapter.kt
@@ -5,24 +5,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.beloushkin.android.learn.notes.R
+import com.beloushkin.android.learn.notes.foundations.BaseRecyclerAdapter
 import com.beloushkin.android.learn.notes.models.Task
 import kotlinx.android.synthetic.main.item_task.view.*
 
 class TaskAdapter(
-    private val taskList: MutableList<Task> = mutableListOf()
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    taskList: MutableList<Task> = mutableListOf()
+) : BaseRecyclerAdapter<Task>(taskList){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
         ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false))
 
-    override fun getItemCount(): Int = taskList.size
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as ViewHolder).onBind(taskList.get(position))
-    }
-
-    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        fun onBind(task: Task) {
-            view.taskTitleView.text = task.title
+    class ViewHolder(view: View) : BaseViewHolder<Task>(view) {
+        override fun onBind(data: Task) {
+            view.titleView.text = data.title
         }
     }
 }
