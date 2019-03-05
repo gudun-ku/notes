c1f5d2f551f07700d5911bf2138f93df040165c1
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-03-05 17:30:44 +0300

    DiffUtil usage for shortening list updates

--- app/src/main/java/com/beloushkin/android/learn/notes/foundations/BaseRecyclerAdapter.kt
+++ app/src/main/java/com/beloushkin/android/learn/notes/foundations/BaseRecyclerAdapter.kt
@@ -1,6 +1,7 @@
 package com.beloushkin.android.learn.notes.foundations
 
 import android.view.View
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
 abstract class BaseRecyclerAdapter<T>(
@@ -8,9 +9,10 @@ abstract class BaseRecyclerAdapter<T>(
 ): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     fun updateList(list: List<T>) {
+        val result = DiffUtil.calculateDiff(DiffUtilCallbackImpl(masterList,list))
         masterList.clear()
         masterList.addAll(list)
-        notifyDataSetChanged()
+        result.dispatchUpdatesTo(this)
     }
 
     override fun getItemViewType(position: Int): Int = if (position == 0) {
@@ -38,5 +40,22 @@ abstract class BaseRecyclerAdapter<T>(
     companion object {
         const val TYPE_ADD_BUTTON = 0
         const val TYPE_INFO = 1
+    }
+
+    //DiffUtil for list changes
+    class DiffUtilCallbackImpl<T>(
+        val oldList: List<T>,
+        val newList: List<T>
+    ): DiffUtil.Callback() {
+        // might be deeper comparison
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean=
+            oldList[oldItemPosition] == newList[newItemPosition]
+
+        override fun getOldListSize(): Int = oldList.size
+
+        override fun getNewListSize(): Int = newList.size
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+                oldList[oldItemPosition] == newList[newItemPosition]
     }
 }
